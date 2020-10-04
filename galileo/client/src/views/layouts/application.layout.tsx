bccb424b0375b27d4047feb7ba7b55d536215822
Kris Quigley <kris@krisquigley.co.uk>
2020-10-04 21:44:59 +0000

    Added sizes using the fibinacci sequence

--- galileo/client/src/views/layouts/application.layout.tsx
+++ galileo/client/src/views/layouts/application.layout.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+// TODO: Should this be imported from index.html instead?
+import 'styles/theme.css';
+
 const ApplicationLayout: React.FC = ({ children }) => {
   return <>{children}</>;
 };
