edac4e5530d7644adfa127ef091a13a88f4fdcd6
Dameon Sumpton <dameon.sumpton@manticpoint.com>
2020-11-27 22:40:56 +0000

    Fix test errors

--- packages/dalton/Tests/anchor.test.tsx
+++ packages/dalton/Tests/anchor.test.tsx
@@ -1,14 +1,15 @@
 import React from "react";
+import '@testing-library/jest-dom/extend-expect'
 
 import { render, screen } from "@testing-library/react";
-
-
 
 import {Anchor} from "../atoms/Anchor";
 
 test("loads and displays Anchor atom", async () => {
-  render( <Anchor /> )
+  render(<Anchor href="bob" />)
 
   screen.debug()
+  
+  expect(screen.getByRole('link')).toHaveTextContent('bob') 
   
 });
